07f4c1ac80761f46701a4fb37475fec58353364d
Ding Lin <tim920323@gmail.com>
2019-10-30 11:30:42 -0400

    array

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array{
+    public static void main(String[]args){
+        int [] arr = new int[20];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = i;
+            System.out .println(arr[i]);
+        }
+        int [] scores = new int []{100,23,123};
+        for(int score : scores){
+            System.out.println(score);
+        }
+    }
+}
